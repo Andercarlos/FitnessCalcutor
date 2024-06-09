ece949e47e5b0040c9d8b9f68ec5fb58d6c3fbc0
Anderson Carlos <you@example.com>
2024-06-09 16:41:48 -0300

    Initial commit

---
+++ app/src/main/java/com/example/fitnesscalcutor/ImcActivity.kt
@@ -0,0 +1,51 @@
+package com.example.fitnesscalcutor
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editPeso : EditText
+    private lateinit var editAltura : EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_imc)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        editPeso = findViewById(R.id.edit_imc_peso)
+        editAltura = findViewById(R.id.edit_imc_altura)
+
+        val btnSend : Button = findViewById(R.id.btn_calc)
+
+        btnSend.setOnClickListener {
+            if(!validation()){
+                Toast.makeText(this, "Não deixe nenhum campo vazio ou iniciando com 0",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+        }
+
+
+    }
+
+    fun validation(): Boolean {
+        return (editPeso.text.toString().isNotEmpty() &&
+            editAltura.text.toString().isNotEmpty() &&
+            !editPeso.text.toString().startsWith("0") &&
+            !editAltura.text.toString().startsWith("0"))
+
+
+    }
+}
---
+++ app/src/main/java/com/example/fitnesscalcutor/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.fitnesscalcutor
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btnImc: LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+
+        btnImc = findViewById(R.id.container_imc)
+
+
+
+        btnImc.setOnClickListener {
+            val i = Intent(this, ImcActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
